9d065048fc93cd367f3795106226826d4ce1901b
Andres Morelos <andresmorelos123@gmail.com>
2019-04-19 12:13:34 -0500

    Updating request implementing the new API specifications

--- src/utils/requets/index.js
+++ src/utils/requets/index.js
@@ -2,7 +2,13 @@ import axios from 'axios';
 
 const get = async url => {
   try {
-    return await axios.get(url);
+    return await axios.get(url,{
+      headers: {
+        "Content-Type": "application/vnd.api+json",
+        "Accept": "application/vnd.api+json;"
+      },
+      data:{}
+    });
   } catch (e) {
     throw e;
   }
