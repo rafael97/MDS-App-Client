61ba1971bcc77c1c96c1bcc95fcde5f19bb84a5f
rafael97 <rafael_9702@hotmail.com>
2019-05-05 21:49:28 -0500

    adding validation label

--- src/components/Pages/Courses/Header.js
+++ src/components/Pages/Courses/Header.js
@@ -1,5 +1,38 @@
 import React from 'react'
+import Label from 'emerald-ui/lib/Label';
+import Progressbar from 'emerald-ui/lib/Progressbar';
 
+
+const validateCourse = (validation) => {
+    let label;
+    let progress;
+    if (!validation) return;
+
+    if (Object.keys(validation).length === 0) {
+        label = <Label style={{ marginRight: '10px' }} color="danger"> No Valido</Label>
+        progress = <Progressbar progress={0} color="danger" circle style={{ marginBottom: '10px' }} />
+    }
+    else if (validation.valid) {
+        console.log(validation.percentaje);
+
+        label = <Label color="success">Cumpliendo</Label>
+        progress = <Progressbar progress={50} color="success" circle style={{ marginBottom: '10px' }} />
+    }
+    else {
+        label = <Label color="danger">No Cumpliendo</Label>
+        progress = <Progressbar progress={validation.percentaje} color="danger" circle style={{ marginBottom: '10px' }} />
+    }
+    return (
+        <div>
+
+            <div className="validation">
+                Programa Labor Docente : {label}
+            </div>
+            <div className="validation">{progress}</div>
+        </div>
+    );
+
+}
 
 
 const Header = (props) => {
@@ -7,6 +40,8 @@ const Header = (props) => {
         <div>
             <h1>{props.course.fullname}</h1>
             <h3>{props.course.shortname}</h3>
+            {validateCourse(props.course.validation)}
+            <Progressbar color="success" progress={25} />
         </div>
     )
 }
